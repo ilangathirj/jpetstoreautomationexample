4260261881cab20cc863e3643edadb7f3246eab7
PiotrW <399piotr@wp.pl>
2020-04-19 14:47:05 +0200

    Add TestRunProperties.java and few things to run tests remote

--- src/test/java/driver/manager/DriverManager.java
+++ src/test/java/driver/manager/DriverManager.java
@@ -1,10 +1,12 @@
 package driver.manager;
 
-import configuration.LocalWebDriverProperties;
 import driver.BrowserFactory;
-import driver.BrowserType;
 import driver.listeners.WebDriverEventListenerRegistrar;
 import org.openqa.selenium.WebDriver;
+
+import static configuration.TestRunProperties.getBrowserToRun;
+import static configuration.TestRunProperties.getIsRemoteRun;
+import static driver.BrowserType.FIREFOX;
 
 public class DriverManager {
 
@@ -16,7 +18,7 @@ public class DriverManager {
     public static WebDriver getWebDriver() {
 
         if (driver == null) {
-            driver = BrowserFactory.getBrowser(LocalWebDriverProperties.getLocalBrowser());
+            driver = new BrowserFactory(getBrowserToRun(), getIsRemoteRun()).getBrowser();
             driver = WebDriverEventListenerRegistrar.registerWebDriverEventListener(driver);
         }
 
@@ -25,7 +27,7 @@ public class DriverManager {
 
     public static void disposeDriver() {
         driver.close();
-        if (!LocalWebDriverProperties.getLocalBrowser().equals(BrowserType.FIREFOX)) {
+        if (!getBrowserToRun().equals(FIREFOX)) {
             driver.quit();
         }
         driver = null;
